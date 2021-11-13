2033c4c4fca78827bb1d3b33f2a66a7363c2b230
arsalan414 <arsalanbaba414@yahoo.com>
2021-11-13 10:05:35 +0500

    assignment02 added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello,<br> I am using React,js</h1>
+      <h1>Hello, I am using Reactjs</h1>
     </div>
   );
 }
